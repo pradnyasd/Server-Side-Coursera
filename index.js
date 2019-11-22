845042e053457b73959a0683cbf5c1c9f5054ffa
pradnyad <pradnya24.dalvi@gmail.com>
2019-11-22 23:01:24 +0530

    Express Simple REST

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname='localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req,res,next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+})
+
+app.get('/dishes', (req,res,next) => {
+    res.end('will send all the dishes to you');
+})
+
+app.post('/dishes',(req,res,next) => {
+    res.end('will add the dish: ' + req.body.name + 
+    '  with details: ' + req.body.description);
+})
+
+app.put('/dishes',(req,res,next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+})
+
+app.delete('/dishes', (req,res,next) => {
+    res.end('Deleting all the dishes!');
+})
+
+app.get('/dishes/:dishId', (req,res,next) => {
+    res.end('will send the details of the dish: '
+    + req.params.dishId + 'to you!');
+})
+    
+app.post('/dishes/:dishIds',(req,res,next) => {
+    res.statusCode = 403;   
+    res.end('POST operation not supported on /dishes/'
+    + req.params.dishId);
+})
+    
+app.put('/dishes/:dishId',(req,res,next) => {
+    res.write('updating the dish: ' + req.params.dishId);
+    res.end('will update the dish: ' + req.body.name + 
+    ' with details: ' + req.body.description);
+})
+    
+app.delete('/dishes/:dishId', (req,res,next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+})    
+    
 
 app.use(express.static(__dirname+ '/public'))
+
 
 app.use((req, res, next) => {
     // console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an express server </h1></body></html>')
-} );
+});
 
 const server = http.createServer(app);
 server.listen(port, hostname,() => {
 console.log(`Server running at http://${hostname}:${port}`)
-})
+});
