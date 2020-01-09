94fcadc83cfb4106a5b1a665c2bc2b97ff7ccf89
pradnyad <pradnya24.dalvi@gmail.com>
2020-01-09 18:53:31 +0530

    HTTPS

--- app.js
+++ app.js
@@ -27,6 +27,17 @@ connect.then((db) => {
 }, (err) => { console.log(err); });
 
 var app = express();
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 var passport = require('passport');
 var authenticate = require('./authenticate');
 // view engine setup
