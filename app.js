cdb84fe7a5bd09ade3ba9467787639000bf0dd2b
pradnyad <pradnya24.dalvi@gmail.com>
2020-01-11 11:17:58 +0530

    Assignment 4: server side

--- app.js
+++ app.js
@@ -86,6 +86,8 @@ app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
 
+app.use('/favorites',favoriteRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
